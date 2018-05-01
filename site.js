777357c5ba99da60e22c27b875ed4b1d0818bfd7
nlw113 <nathanworford@gmail.com>
2018-04-30 20:11:15 -0600

    symbols added

--- site.js
+++ site.js
@@ -65,3 +65,22 @@ function smoothscroll(elem, ease=0.1){
     scroll();
 }
 
+//-------------------------- carousel --------------------------
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
+}
+
